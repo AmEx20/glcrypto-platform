48d79fd13c7819b865a5d646c093fde5c0eba664
AmEx20 <66038757+AmEx20@users.noreply.github.com>
2026-03-25 14:45:21 +0200

    Create Home Page for Next.js application

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Crypto Platform</h1>
+            <p>Your one-stop solution for cryptocurrency management.</p>
+        </div>
+    );
+};
+
+export default HomePage;
